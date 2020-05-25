6a9917861431945563a8de982209d8dffcd43b83
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-25 16:14:52 +0100

    Changes to interests page

--- src/Interests/Interests.js
+++ src/Interests/Interests.js
@@ -1,35 +1,41 @@
 import React from "react";
 import "./Interests.css";
-import html from "../Imgs/Logos/html.jpeg";
-import javaScript from "../Imgs/Logos/javaScript.png";
-import react from "../Imgs/Logos/react.png";
-import css from "../Imgs/Logos/css.png";
 import { Link } from "react-router-dom";
+import Navbar from "../Navbar/Navbar.js";
+import grad from "../Imgs/grad.JPG";
+import bali from "../Imgs/bali.JPG";
 
 export default function Interests() {
   return (
     <div>
+      <Navbar />
       <div className="interests">
-        <img src={javaScript} alt="javaScript" />
-        <img src={react} alt="react" />
-        <img src={html} alt="html" />
-        <img src={css} alt="css" />
-        <p className="info">
+        <h1 className="title">About me</h1>
+        <img className="grad" src={grad} alt="graduation" />
+        <p className="education">
           Hi, I’m Lauren, a Junior Full Stack Software Developer based in
           London. I graduated from the University of Warwick in 2018 with a
           degree in German, and my passion for language-learning has since
-          extended into those of the programming variety. After spending a year
-          practising JavaScript in my free time, I am now enrolled on Kodiri’s
-          intensive Bootcamp, learning JavaScript, HTML and CSS as well as
-          ReactJs and NodeJs. <br /> <br />
+          extended into those of the programming variety.
+          <br />
+          <br />
+          After spending a year practising JavaScript in my free time, I am now
+          enrolled on Kodiri’s intensive Bootcamp, learning JavaScript, HTML and
+          CSS as well as ReactJs and NodeJs.
+        </p>
+        <p className="hobbies">
           I’m looking to work for a company that would benefit from an ambitious
           Junior Developer, and who would support a fast and dedicated learner
-          grow and expand their skills. Aside from programming, I enjoy spending
-          my time getting active outdoors, and socialising with friends and
-          family. I love travelling, particularly when it involves some sort of
-          adventure, particularly mountain-climbing.
+          grow and expand their skills.
+          <br />
+          <br />
+          Aside from programming, I enjoy spending my time getting active
+          outdoors, and socialising with friends and family. I love travelling,
+          particularly when it involves some sort of adventure, particularly
+          mountain-climbing.
         </p>
-        <Link className="detailsLink" to="/">
+        <img className="bali" src={bali} alt="mountain-climbing" />
+        <Link className="link" to="/">
           Back to homepage
         </Link>
       </div>
