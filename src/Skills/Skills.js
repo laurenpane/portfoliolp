b0d3a2d012ab55b9553b6c39c81108fc643d2fb9
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-26 17:38:29 +0100

    Test- removed JS

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Skills.css";
-import javaScript from "../Imgs/Logos/javascript.png";
 import react from "../Imgs/Logos/react.png";
 import html from "../Imgs/Logos/html.png";
 import css from "../Imgs/Logos/css.png";
@@ -11,7 +10,6 @@ export default function Skills() {
     <div className="skillsPage">
       <h1 className="title">My skills</h1>
       <div className="skills">
-        <img className="javaScript" src={javaScript} alt="javaScript" />
         <img className="react" src={react} alt="react" />
         <img className="nodejs" src={nodejs} alt="nodejs" />
         <img className="html" src={html} alt="html" />
