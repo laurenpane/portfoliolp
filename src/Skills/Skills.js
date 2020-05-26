806d2edad2692bcf8330e9db32ed599410d85818
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-26 17:43:51 +0100

    Test - readded Javascript

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -4,12 +4,14 @@ import react from "../Imgs/Logos/react.png";
 import html from "../Imgs/Logos/html.png";
 import css from "../Imgs/Logos/css.png";
 import nodejs from "../Imgs/Logos/nodejs.png";
+import javascript from "../Imgs/Logos/javascript.png";
 
 export default function Skills() {
   return (
     <div className="skillsPage">
       <h1 className="title">My skills</h1>
       <div className="skills">
+        <img className="javaScript" src={javascript} alt="javaScript" />
         <img className="react" src={react} alt="react" />
         <img className="nodejs" src={nodejs} alt="nodejs" />
         <img className="html" src={html} alt="html" />
