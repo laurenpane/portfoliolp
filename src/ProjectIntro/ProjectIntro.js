53fa1e1f0515ad94df5144b83f7053fac981cb7e
Lauren Pane <lauren.pane1@googlemail.com>
2020-06-13 19:01:52 +0100

    Made changes to project intro on homepage, and product page. Added 'Carrate' and changed Kodflix logo

--- src/ProjectIntro/ProjectIntro.js
+++ src/ProjectIntro/ProjectIntro.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import kodflixLogo from "../Imgs/Logos/netflix.png";
+import kodflixLogo from "../Imgs/Logos/kodflix.JPG";
+import carrateLogo from "../Imgs/carrate.jpg";
 import "./ProjectIntro.css";
 
 export default function ProjectIntro() {
   return (
     <div data-aos="fade">
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+        rel="stylesheet"
+      />
       <div className="projectIntro">
         <h1 className="pLinkTitle" data-aos="fade in">
           My projects
@@ -16,10 +21,18 @@ export default function ProjectIntro() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={kodflixLogo} className="kLinkImg" alt="netflix logo" />
+          <img src={kodflixLogo} className="kLinkLetter" alt="carrate logo" />
+        </a>
+        <a
+          href="https://carrate.herokuapp.com/"
+          className="carrateLink"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={carrateLogo} className="cLinkImg" alt="carrate logo" />
         </a>
         <Link className="more-projects" to="/projects">
-          Read about my projects
+          <h3>Read about my projects</h3>
         </Link>
       </div>
     </div>
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Projects.css";
 import Navbar from "../Navbar/Navbar.js";
-import kodflixHomeScreenshot from "../Imgs/Logos/netflix.png";
+import kodflixHomeScreenshot from "../Imgs/Logos/kodflix.JPG";
+import carrateHomeScreenshot from "../Imgs/carrate.jpg";
 
 export default function Projects() {
   return (
@@ -21,7 +22,12 @@ export default function Projects() {
         <div className="kCaption">
           <p>
             Full-stack web application simulating Netflix, using ReactJs, CSS,
-            JavaScript and NodeJs, compatible with desktop and mobile.
+            JavaScript and NodeJs. Users can browse television programmes from
+            an animated grid, and select any of their choice to discover more
+            information about that series. Data is fetched from a backend
+            source, and is deployed using Heroku. This application is compatible
+            with desktop and mobile. This project was built during the course of
+            the Kodiri.com bootcamp, in May-June 2020.
           </p>
           <a
             className="kLink"
@@ -31,6 +37,35 @@ export default function Projects() {
           >
             Check out Kodflix
           </a>
+        </div>
+        <div className="carrate">
+          <h2 className="kHeader">carrate</h2>
+          <img
+            className="carrateHome"
+            src={carrateHomeScreenshot}
+            alt="carrateHome"
+          />
+          <div className="kCaption">
+            <p>
+              Full-stack, mobile-first recipe-finding application, built with
+              ReactJs, CSS, JavaScript and NodeJs. The user enters all
+              ingredients they wish to use into the searchbar, and the
+              application returns recipes from an API. Recipes can be filtered
+              by dietary requirements, and display nutritional information, such
+              as calories. The user also has the option to select the 'Inspire
+              Me' button, which generates a random recipe result. This project
+              was built collaboratively during the course of the Kodiri.com
+              bootcamp, in June 2020.
+            </p>
+            <a
+              className="kLink"
+              href="https://carrate-lpane.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Check out carrate
+            </a>
+          </div>
         </div>
       </div>
       <div className="kHomeContainer">
