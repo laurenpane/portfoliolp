0c0d4a8d91e92bfd7f9d0decd6048a3e4fb34ee0
Lauren Pane <lauren.pane1@googlemail.com>
2020-06-16 12:28:12 +0100

    Resized Kodflix logo on ProjectsIntro and sorted alignment

--- src/ProjectIntro/ProjectIntro.js
+++ src/ProjectIntro/ProjectIntro.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import kodflixLogo from "../Imgs/Logos/kodflix.JPG";
+import kodflixLogo from "../Imgs/Logos/kodflix.png";
 import carrateLogo from "../Imgs/carrate.jpg";
 import "./ProjectIntro.css";
 
