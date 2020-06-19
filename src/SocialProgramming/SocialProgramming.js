b054211fb5b39a54beecbaac07914ad11b682ef9
Lauren Pane <lauren.pane1@googlemail.com>
2020-06-19 14:51:43 +0100

    Added title and descriptions to social programming

--- src/SocialProgramming/SocialProgramming.js
+++ src/SocialProgramming/SocialProgramming.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SocialProgramming.css";
 import javascript from "../Imgs/Logos/js2.png";
+import programming from "../Imgs/Logos/programming.png";
 
 export default function SocialProgramming() {
   return (
@@ -9,6 +10,13 @@ export default function SocialProgramming() {
         <h1 className="social-programming-title">Social Programming</h1>
       </div>
       <div className="social-programming-grid">
+        <h3 className="social-programming-item title">Recursion Workshop</h3>
+        <h3 className="social-programming-item title">
+          Live JavaScript and React Coding Event - Kodiri
+        </h3>
+        <h3 className="social-programming-item title">
+          Live React Coding Event - Time to React
+        </h3>
         <a
           className="social-programming-item"
           href="https://www.youtube.com/watch?v=uFXJqUmmI20&list=PL8xuokhAnn4qKWYQFq012dq1aFW5fIGr4"
@@ -19,11 +27,11 @@ export default function SocialProgramming() {
         </a>
         <a
           className="social-programming-item"
-          href="https://www.youtube.com/watch?v=9v579YUtTA8"
+          href="https://www.kodiri.com/news/rewatch-our-social-coding-session-with-giacomo-sorbi-and-pusher"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={javascript} className="social-link-img" alt="recursion" />
+          <img src={programming} className="social-link-img" alt="recursion" />
         </a>
         <a
           className="social-programming-item"
@@ -31,8 +39,20 @@ export default function SocialProgramming() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={javascript} className="social-link-img" alt="recursion" />
+          <img src={programming} className="social-link-img" alt="recursion" />
         </a>
+        <p className="social-programming-item">
+          During the Kodiri bootcamp, I researched and presented a workshop
+          about my chosen topic, Recursion.
+        </p>
+        <p className="social-programming-item">
+          Participated in a live JavaScript and React coding event, where I
+          worked with a team, including Giacomo Sorbi, to solve questions.
+        </p>
+        <p className="social-programming-item">
+          Live React event, where I participated amongst a small group of fellow
+          Kodiri students.
+        </p>
       </div>
     </div>
   );
